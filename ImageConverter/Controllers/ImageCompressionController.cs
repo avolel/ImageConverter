c5cc0dca0adab9259e5517e178647b14aa22a0e7
Andre Volel <avolel@gmail.com>
2025-09-29 15:01:53 -0400

    rem using

--- ImageConverter/Controllers/ImageCompressionController.cs
+++ ImageConverter/Controllers/ImageCompressionController.cs
@@ -1,12 +1,6 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Jpeg;
-using SixLabors.ImageSharp.Processing;
-
 namespace ImageConverter.Controllers
 {
     [ApiController]
